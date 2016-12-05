637a668097dae037096a3353dd81adcdf7539fa0
Shane Sarnac <shane.sarnac@colorado.edu>
2016-12-04 21:08:34 -0700

    Small and medium ball

--- phase_velocities.cpp
+++ phase_velocities.cpp
@@ -18,8 +18,35 @@ double phase_speed(double kappa) {
 
 int main() {
 	cout << "kappa, group speed, phase speed" << endl; 
-	for (int i = 1; i < 1000; i++) {
+	/** for (int i = 1; i < 1000; i++) {
 		cout << i << "," << group_speed(i) << "," << phase_speed(i) << endl;
 		
-	}
+	}**/
+	
+	// small ball (diameter 0.0824 inch)
+	double kappa = 852.42;
+	/**cout << kappa << "," << group_speed(kappa) << "," << phase_speed(kappa) << endl;
+	
+	kappa = 1125.62;
+	cout << kappa << "," << group_speed(kappa) << "," << phase_speed(kappa) << endl;
+	
+	kappa = 1187.53;
+	cout << kappa << "," << group_speed(kappa) << "," << phase_speed(kappa) << endl;
+	
+	kappa = 989.48;
+	cout << kappa << "," << group_speed(kappa) << "," << phase_speed(kappa) << endl;**/
+	
+	// medium ball (diameter 0.618 inch)
+	kappa = 747.55;
+	cout << kappa << "," << group_speed(kappa) << "," << phase_speed(kappa) << endl;
+	
+	kappa = 774.36;
+	cout << kappa << "," << group_speed(kappa) << "," << phase_speed(kappa) << endl;
+	
+	kappa = 973.84;
+	cout << kappa << "," << group_speed(kappa) << "," << phase_speed(kappa) << endl;
+	
+	kappa = 864.86;
+	cout << kappa << "," << group_speed(kappa) << "," << phase_speed(kappa) << endl;
+	
 }
